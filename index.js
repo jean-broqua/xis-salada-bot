e30ca6a9041be6653e7fc995156e9e082a6b32c0
Jean Broqua <js.broqua@gmail.com>
2020-08-29 23:57:37 -0300

    intial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+const client = new Discord.Client();
+
+const xisCotation = config.cotation;
+const prefix = config.prefix;
+
+client.once('ready', () => {
+    console.log('Xis Salada in online');
+});
+
+client.on('message', message => {
+    if(message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(message.content.search('xis') != -1) {
+        message.reply('oi');
+    }
+
+    if(command === 'converter') {
+        const amount= parseFloat(args[0]) / xisCotation;
+
+        if (!isNaN(amount)) {
+            message.channel.send('R$ ' + args[0] + ' = ' + 'X$' + amount.toFixed(2));
+        }
+    } else if(command == '') {
+        
+    }
+});
+
+client.login(process.env.TOKEN);
